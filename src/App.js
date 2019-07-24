f4f79cfaf82926632214c34576dc4ed40859ed8c
baxter2 <baxcallan@gmail.com>
2019-07-24 14:46:20 +1000

    udpate

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ class App extends Component {
        .then(users => this.setState({monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -32,7 +36,7 @@ class App extends Component {
       <div className="App">
 	<SearchBox
           placeholder="search box"
-	  handleChange={e => this.setState({ searchField: e.target.value })}
+	  handleChange={this.handleChange}
 	/>
 
         <CardList monsters={filteredMonsters} />

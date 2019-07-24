c5aa2574ddecb9398fd5194561513f6110818ba6
baxter2 <baxcallan@gmail.com>
2019-07-24 15:08:57 +1000

    Updates

---
+++ precache-manifest.48c21dfa5053072daf9483c10876d3ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740aa5902a2c1e71b09d7810e5a6ae52",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8a9adbfe066250391017",
+    "url": "/monsters-rolodex/static/css/main.daa9db8e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8a9adbfe066250391017",
+    "url": "/monsters-rolodex/static/js/main.50f33a22.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
